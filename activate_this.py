a949c1dba003985874b9036bb4da40e6f5f49f1c
gespressbrasil <mulleradm@icloud.com>
2025-01-05 17:45:05 -0300

    Update activate_this.py

--- activate_this.py
+++ activate_this.py
@@ -1,11 +1,7 @@
-# activate_this.py
 import os
 import sys
 
 activate_this = '/var/www/cofre-da-sorte/venv/bin/activate_this.py'
-
-# Alterar o caminho do sistema para apontar para o ambiente virtual
-sys.path.insert(0, '/var/www/cofre-da-sorte/venv/lib/python3.12/site-packages')
 
 # Ativa o ambiente virtual
 exec(open(activate_this).read(), {'__file__': activate_this})
