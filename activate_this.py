c0c7f7168d44efdc483f0895c0ee34c33ffd0ea5
gespressbrasil <mulleradm@icloud.com>
2025-01-05 18:51:47 -0300

    Update activate_this.py

--- activate_this.py
+++ activate_this.py
@@ -1,7 +1,12 @@
 import os
 import sys
 
+# Caminho para o ambiente virtual
 activate_this = '/var/www/cofre-da-sorte/venv/bin/activate_this.py'
 
-# Ativa o ambiente virtual
-exec(open(activate_this).read(), {'__file__': activate_this})
+# Verifica se o arquivo de ativação do ambiente virtual existe
+if os.path.exists(activate_this):
+    with open(activate_this) as f:
+        exec(f.read(), {'__file__': activate_this})
+else:
+    sys.exit("O arquivo 'activate_this.py' não foi encontrado!")
